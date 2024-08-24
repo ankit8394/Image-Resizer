27ad9279d3ec771d9c57a1cfbbfff6d667799ca3
Ankit <ankit0839483@gmail.com>
2024-08-24 23:57:18 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+
+        let h=0;
+        let w=0;
+        let borw=0;
+        let bors=0;
+        let borc=0;
+        function rsz()
+        {
+            h=document.getElementById("hh").value;
+            w=document.getElementById("ww").value;
+            borw=document.getElementById("bw").value;
+            bors=document.getElementById("bs").value;
+            borc=document.getElementById("bc").value;
+            document.getElementById("img").style.height=h+"px";
+            document.getElementById("img").style.width=w+"px";
+            document.getElementById("img").style.borderWidth=borw+"px";
+            document.getElementById("img").style.borderStyle=bors;
+            document.getElementById("img").style.borderColor=borc;
+        }
